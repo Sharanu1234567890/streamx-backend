5d506c81cf0b3799ccfab19229ecf771b33c0e05
User3 <user3@example.com>
2026-05-24 22:13:32 +0530

    day2: api gateway working, auth routes accessible through port 8080

---
+++ streamx/api-gateway/src/main/java/com/streamx/gateway/filter/conf/GatewaySecurityConfig.java
@@ -0,0 +1,20 @@
+package com.streamx.gateway.filter.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex -> ex.anyExchange().permitAll())
+                .build();
+    }
+}
